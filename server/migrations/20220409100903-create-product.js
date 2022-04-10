fe967e14c135a15be677174e285afa449ff60787
Essa <essa.faizal@mail.ugm.ac.id>
2022-04-10 13:54:06 +0700

    add relation: user->profile

---
+++ server/migrations/20220409100903-create-product.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      productId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      brand: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.INTEGER
+      },
+      kilometers: {
+        type: Sequelize.INTEGER
+      },
+      grade: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      delete: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ server/models/product.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    brand: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    kilometers: DataTypes.INTEGER,
+    grade: DataTypes.STRING,
+    category: DataTypes.STRING,
+    description: DataTypes.STRING,
+    delete: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
