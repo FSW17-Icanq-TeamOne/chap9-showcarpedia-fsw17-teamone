80a65e80f5e702a787392df3ea567c6f35c610dc
Jeremiah Prasetyo <93666214+jepra777@users.noreply.github.com>
2022-04-12 16:02:57 +0700

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-module.exports = {
+require("dotenv").config()
+module.exports={
   "development": {
-    "username": process.env.DB_USERNAME_DEV || "postgres",
-    "password": process.env.DB_PASSWORD_DEV || "postgres",
-    "database": process.env.DB_DATABASE_DEV || "showcarpedia",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
