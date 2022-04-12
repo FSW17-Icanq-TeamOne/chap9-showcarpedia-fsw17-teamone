e7514092c948aeac35e2d6dd6a58d2327507cd8e
kelvin handoko <kelvinhandoko03@gmail.com>
2022-04-13 02:14:17 +0700

    feat:create profile controller

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,5 +1,40 @@
-class ProfileController{
-    
+const {
+    Profile
+} = require("../models")
+
+class ProfileController {
+    static async createProfile(req, res) {
+        //  ini tunggu auth jadi dulu
+        const userId = req.user.id
+
+        const {
+            fullName,
+            birthDate,
+            city,
+            country,
+            mobilePhone,
+            profilePicture
+        } = req.body
+        const payload = {
+            fullName,
+            birthDate,
+            city,
+            country,
+            mobilePhone,
+            profilePicture,
+        }
+        try {
+            if (!userId) return res.json("user not found")
+            const data = await Profile.create(payload, {
+                where: {
+                    userId
+                }
+            })
+            res.status(200).json(data)
+        } catch (error) {
+            throw(error)
+        }
+    }
 }
 
 module.exports = ProfileController
---
+++ server/routes/v1/profileRouter.js
@@ -0,0 +1,8 @@
+const profileRouter = require("express").Router()
+const {ProfileController} = require("../../controllers")
+
+profileRouter.get("/",)
+
+
+
+module.exports = profileRouter
