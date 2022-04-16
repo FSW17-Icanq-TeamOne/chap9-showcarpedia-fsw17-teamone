f548989279411913d1b0180e7f324b5ddbaa9dbd
Essa <essa.faizal@mail.ugm.ac.id>
2022-04-16 21:42:42 +0700

    get Wishlist by Id

--- server/controllers/wishlistController.js
+++ server/controllers/wishlistController.js
@@ -21,66 +21,27 @@ class wishlistController {
         }
     }
 
-    // static async getAllProduct(req,res){
-    //     try {
-    //         const data = await Product.findAll({order:[['id',"ASC"]]})
-    //         if(!data.length) res.json("please add new product")
-    //         res.status(200).json(data)
-    //     } catch (error) {
-    //         throw error
-    //     }
-    // }
 
-    // static async getProductById(req, res){
-    //     try{
-    //         const { idProduct } = req.params
-    //         const data = await Product.findByPk(idProduct)
-    //         if (data){
-    //             return res.status(200).json({
-    //                 result: "Success",
-    //                 data: data
-    //             });
-    //         }    
-    //     } catch (error){
-    //         throw error
-    //     }
-    // }
+    static async getWishlistById(req, res){
+        try{
+            const { id } = req.params
+            const data = await Wishlist.findAll({
+                where: {
+                    userId: id
+                }
+            })
+            if (data){
+                return res.status(200).json({
+                    result: "Success",
+                    data: data
+                });
+            }    
+            
+        } catch (error){
+            throw error
+        }
+    }
 
-    // static async updateProductById(req, res){
-    //     try{
-    //         const { id } = req.params
-    //         const {
-    //             title,
-    //             brand,
-    //             year,
-    //             kiloMeter,
-    //             grade,
-    //             category,
-    //             description,
-    //             photoProduct
-    //         } = req.body
-    //         const updateDataCars = {
-    //             title,
-    //             brand,
-    //             year,
-    //             kiloMeter,
-    //             grade,
-    //             category,
-    //             description,
-    //             delete: false,
-    //             photoProduct
-    //         }
-    //         const data = await Product.update(updateDataCars, { where: { id: id } })
-    //         if (data == 1){
-    //             return res.status(200).json({
-    //                 message: "updated!", 
-    //                 data: req.body
-    //             })
-    //         }
-    //     } catch (error){
-    //         throw error
-    //     }
-    // }
     static async deleteWishlist (req, res){
         try {
             const { id } = req.body
