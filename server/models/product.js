31cc6fbad1104ffb7ba2b8e79fb662bea564d3df
Jeremiah Prasetyo <93666214+jepra777@users.noreply.github.com>
2022-04-16 20:22:00 +0700

    Update product.js

--- server/models/product.js
+++ server/models/product.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      //Product.belongsTo(models.User);
+      //Product.belongsTo(models.User)
       Product.belongsToMany(models.User, { through: models.Wishlist, foreignKey: "productId"})
     }
   }
