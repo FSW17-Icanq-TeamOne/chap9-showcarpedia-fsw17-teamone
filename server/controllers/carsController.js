69619bb8771a2c8901e3ed6fd51630fe72df3db9
Essa <essa.faizal@mail.ugm.ac.id>
2022-04-16 11:45:24 +0700

    add get all products

--- server/controllers/carsController.js
+++ server/controllers/carsController.js
@@ -26,5 +26,15 @@ class carsController {
             })
         }
     }
+
+    static async getAllProduct(req,res){
+        try {
+            const data = await Product.findAll({order:[['id',"ASC"]]})
+            if(!data.length) res.json("please add new product")
+            res.status(200).json(data)
+        } catch (error) {
+            throw error
+        }
+    }
 }
 module.exports = carsController
