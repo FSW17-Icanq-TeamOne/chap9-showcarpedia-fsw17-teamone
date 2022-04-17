f94a735c298ac49144641226e54ccf04be8e2d47
Jeremiah Prasetyo <93666214+jepra777@users.noreply.github.com>
2022-04-17 15:29:19 +0700

    Update routes.js

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -5,8 +5,8 @@ const RegisterAccount = React.lazy(() => import('../pages/RegisterAccount'))
 const RegisterProfile = React.lazy(() => import('../pages/RegisterProfile')) 
 
 const Collection = React.lazy(() => import('../pages/Collection'))
-const DetailProduct = React.lazy(() => import('../pages/DetailProduct'))
-
+//const DetailProduct = React.lazy(() => import('../pages/DetailProduct'))
+const CollectionDetail = React.lazy(() => import('../pages/CollectionDetail'))
 
 const routes = [
     { path: '/login', element: <Login /> },
