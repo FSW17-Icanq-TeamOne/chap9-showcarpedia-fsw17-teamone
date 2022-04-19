bf7e870a717edcf3119c33b7b11d4d9bde5096ad
Alexander Gozali <alexandergozali0@gmail.com>
2022-04-19 15:28:32 +0700

    Feat: Product Creation Proto

--- client/src/components/ProductCreationForm.jsx
+++ client/src/components/ProductCreationForm.jsx
@@ -1,7 +1,69 @@
-import { Grid } from "@mui/material"
+import { Button, Grid } from "@mui/material"
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { useFormik } from "formik"
+import { useEffect, useState } from "react"
+import { storage } from "../config/firebase";
 
 export default function ProductCreationForm() {
+    const [image, setImage] = useState();
+    const [preview, setPreview] = useState();
+    const [dwnldUrl, setDwnldUrl] = useState();
+
+    useEffect(() => {
+        if (!image) {
+            setPreview('https://via.placeholder.com/600x400/ff0000/ffffff?text=Hello+MF')
+            return;
+        }
+        const objURL = URL.createObjectURL(image)
+        setPreview(objURL)
+        return () => URL.revokeObjectURL(objURL)
+    }, [image])
+
+    const handleChange = (event) => {
+        if (!event.target.file || event.target.files.length === 0) {
+            setImage(undefined)
+
+        }
+        setImage(event.target.files[0])
+    }
+
+    const handleUpload = (event) => {
+        const storageRef = ref(storage, `images/${image.name}`);
+
+        const uploadTask = uploadBytesResumable(storageRef);
+
+        uploadTask.on('state_changed', (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            
+            console.log(`File upload is ` + progress + ` % done`);
+
+            switch (snapshot.state) {
+                case 'paused':
+                    console.log('Upload is Paused')
+                    break;
+                case 'running':
+                    console.log('Upload is Running')
+                    break;
+                case 'error':
+                    console.log('Upload is Error');
+                    break;
+                case 'canceled':
+                    console.log('Upload is Cancelled');
+                    break;
+                default:
+                    break;
+            }
+        }, (error) => {
+            console.log(error)
+        }, () => {
+            getDownloadURL(uploadTask.snapshot.ref)
+                .then((downloadUrl) => {
+                    console.log('file available at', downloadUrl)
+                    setDwnldUrl(dwnldUrl);
+                })
+        })
+    }
+    
     const formik = useFormik({
         initialValues: {
             title: '',
@@ -15,10 +77,13 @@ export default function ProductCreationForm() {
             console.log(values)
         }
     })
+
     return (
         <>
             <Grid>
-                <Grid />
+                <img src={preview} alt='image' style={{ width: 300 }}/>
+                <input type='file' onChange={handleChange} />
+                <Button onClick={handleUpload}>Upload</Button>
             </Grid>
         </>
     )
