1861c9c0072eb5cb5720b191edbffd5cf03481f9
Jeremiah <jere.prasetyo.jp@gmail.com>
2022-04-20 17:05:06 +0700

    FE BE Combine Create Product Features upload Firebase

--- client/src/components/ProductCreationForm.jsx
+++ client/src/components/ProductCreationForm.jsx
@@ -1,20 +1,123 @@
-import { Autocomplete, Button, Grid, TextField } from "@mui/material";
+import { Box, FormControl, Select, InputLabel, MenuItem, Autocomplete, Button, Grid, TextField } from "@mui/material";
 import { useFormik } from "formik";
-import MultipleImageUploader from "./MultipleImageUploader";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { useEffect, useState } from "react";
+import { storage } from '../config/firebase'
+import { useNavigate } from "react-router-dom";
 
 export default function ProductCreationForm() {
+
+    const navigate = useNavigate()
+
+    const [images, setImages] = useState([]);
+    const [previews, setPreviews] = useState([]);
+    const [urls, setUrls] = useState([]);
+    const [progress, setProgress] = useState(0);
+    const dataURL = []
+
+    useEffect(() => {
+        if (!images) {
+            setPreviews([
+                'https://via.placeholder.com/600x400/f28329/ffffff?text=Upload+Image',
+                'https://via.placeholder.com/600x400/f28329/ffffff?text=Upload+Image',
+                'https://via.placeholder.com/600x400/f28329/ffffff?text=Upload+Image',
+                'https://via.placeholder.com/600x400/f28329/ffffff?text=Upload+Image'
+            ]);
+            return;
+        }
+        images.map(image => {
+            const objURL = URL.createObjectURL(image);
+            setPreviews(prev => [...prev, objURL]);
+            
+            return () => URL.revokeObjectURL(objURL);
+        });
+    }, [images]);
+
+    const handleChange = (e) => {
+        for (let i = 0; i < e.target.files.length; i++) {
+            const newImage = e.target.files[i];
+
+            newImage.id = Math.random();
+
+            setImages(prevState => [...prevState, newImage]);
+        }
+    };
+
+    const handleUpload = () => {
+        const promises = [];
+
+        images.map(image => {
+            image.nameFile = new Date() + image.name;
+            const storageRef = ref(storage, `multipleImages/${image.nameFile}`);
+
+            const uploadTask = uploadBytesResumable(storageRef, image);
+            promises.push(uploadTask);
+            
+            uploadTask.on(
+                `state_changed`,
+                snapshot => {
+                    const prog = Math.round(
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    );
+                    setProgress(prog)
+                },
+                error => {
+                    console.log(error)
+                },
+                async () => {
+                    await getDownloadURL(uploadTask.snapshot.ref)
+                        .then(downloadURL => {
+                            setUrls(prev => [...prev, downloadURL]);
+                            console.log(`file available at `, downloadURL);
+                            dataURL.push(downloadURL)
+                            console.log("ini", dataURL)
+                            formik.setFieldValue("photoProducts", dataURL)
+                        })
+                        .catch(error => console.log(error));
+                }
+            );
+        });
+        Promise.all(promises)
+            .then(() => alert('File is successfully uploaded'))
+            .catch(error => console.log(error))
+
+        
+    }
+
     const formik = useFormik({
         initialValues: {
             title: '',
-            brand: ['Porsche', 'BMW', 'Ford', 'Toyota', 'Honda'],
-            grade: [1, 2, 3, 4, 5],
-            category: ['Sport', 'SUV', 'Pickup', 'Coupe',],
-            year: Number(),
-            kiloMeter: Number(),
-            description: ''
+            brand: '',
+            grade: '',
+            category: '',
+            year: '',
+            kiloMeter: '',
+            description: '',
+            photoProducts: ''
         },
         onSubmit: value => {
             console.log(value, `Product is Created`)
+            fetch('http://localhost:4000/v1/cars', {
+                method: 'POST',
+                headers: { 
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json' },
+                credentials: "include",
+                body: JSON.stringify(value),
+            })
+            .then((response) => {
+            return response.json();
+            })
+            .then((data) => {
+            console.log(data, 'This is the Data')
+            if (data.message === "Success"){
+                navigate("/productList")
+                window.location.reload();
+            }
+            })
+            .catch((err) => {
+            console.log(err);
+            });
         }
     })
 
@@ -22,9 +125,62 @@ export default function ProductCreationForm() {
         <>
         {/* Form */}
         <Grid display={'flex'} gap={'30px'}>
-            <MultipleImageUploader />
+            {/* Picture */}
+            <Grid container display={'flex'} justifyContent={'center'} alignContent={'center'} 
+                sx={{
+                    width: 500,
+                    height: 500,
 
-            <form onSubmit={formik.onSubmit}>
+                    border: 1
+                }}
+            >
+                {/* Image Viewer */}
+                <Grid display={'flex'} justifyContent={'center'} alignItems={''}
+                    sx={{
+                        width: 400,
+                        height: 400
+                    }}
+                >
+                    {urls.map((url, idx) => (
+                        <img key={idx} style={{maxHeight: 100, maxWidth: 100}} src={url} lat={url} />
+                    ))}
+                </Grid>
+
+                <Grid display={'flex'} justifyContent={'center'} alignItems={'baseline'}>
+                    <progress 
+                        value={progress} 
+                        max='100%' 
+                        style={{
+                            width: 400
+                        }}
+                    />
+
+                    {
+                        previews.map((image, idx) => {
+                            <img 
+                                src={image}
+                                key={idx}
+                                style={{}}
+                                alt={image.nameFile}
+                            />
+                        })
+                    }
+                </Grid>
+
+                <Grid display={'flex'} alignItems={'center'} justifyContent={'center'}>
+                    <input type={'file'} multiple onChange={handleChange} />
+                    <Button sx={{
+                        width: 150,
+                        
+                        borderRadius: 20,
+
+                        backgroundColor: 'orange',
+                        color: 'white'
+                    }} onClick={handleUpload}>Upload</Button>
+                </Grid>
+            </Grid>
+
+            <form onSubmit={formik.handleSubmit}>
                 <Grid container display={'flex'} direction={'column'} gap={'30px'}>
 
                     <Grid>
@@ -44,52 +200,71 @@ export default function ProductCreationForm() {
                     </Grid>
 
                     <Grid display={'flex'} gap={'20px'}>
-                        <Autocomplete 
-                            disablePortal
-                            id="brand"
-                            name='brand'
-                            
-                            onChange={formik.handleChange}
+                        <Box sx={{ minWidth: 120 }}>
+                        <FormControl fullWidth>
+                        <InputLabel>Brand</InputLabel>
+                        <Select
+                        
+                        id="brand"
+                        name="brand"
+                        sx={{
+                            width: 345
+                        }}
+                        onChange={formik.handleChange}
+                        >
+                        <MenuItem value={"Porsche"}>Porsche</MenuItem>
+                        <MenuItem value={"BMW"}>BMW</MenuItem>
+                        <MenuItem value={"Ford"}>Ford</MenuItem>
+                        <MenuItem value={"Toyota"}>Toyota</MenuItem>
+                        <MenuItem value={"Honda"}>Honda</MenuItem>
+                        </Select>
+                        </FormControl>
+                         </Box>
 
-                            options={formik.values.brand}
-                            renderInput={params => <TextField {...params} label='Select Brand' required />}
+                         <Box sx={{ minWidth: 120 }}>
+                        <FormControl fullWidth>
+                        <InputLabel>Grade</InputLabel>
+                        <Select
+                        id="grade"
+                        name="grade"
+                        sx={{
+                            width: 95
+                        }}
+                        onChange={formik.handleChange}
+                        >
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                        <MenuItem value={4}>4</MenuItem>
+                        <MenuItem value={5}>5</MenuItem>
+                        </Select>
+                        </FormControl>
+                         </Box>
 
-                            sx={{
-                                width: 345
-                            }}
-                        />
-
-                        <Autocomplete 
-                            disablePortal
-                            id="grade"
-                            name='grade'
-                            
-                            onChange={formik.handleChange}
-
-                            options={formik.values.grade}
-                            renderInput={params => <TextField {...params} label='Grade' required />}
-
-                            sx={{
-                                width: 95
-                            }}
-                        />
+                        
                     </Grid>
 
                     <Grid>
-                        <Autocomplete 
-                            disablePortal
-                            id="category"
-                            name='category'
-                            
-                            onChange={formik.handleChange}
-
-                            options={formik.values.category}
-                            renderInput={params => <TextField {...params} label='Category' required />}
-
-                            sx={{
-                                width: 345
-                            }}
-                        />
+                        <Box sx={{ minWidth: 120 }}>
+                        <FormControl fullWidth>
+                        <InputLabel>Category</InputLabel>
+                        <Select
+                        
+                        id="category"
+                        name='category'
+                        sx={{
+                            width: 345
+                        }}
+                        onChange={formik.handleChange}
+                        >
+                        <MenuItem value={"Sport"}>Sport</MenuItem>
+                        <MenuItem value={"SUV"}>SUV</MenuItem>
+                        <MenuItem value={"Pickup"}>Pickup</MenuItem>
+                        <MenuItem value={"Coupe"}>Pickup</MenuItem>
+                        </Select>
+                        </FormControl>
+                         </Box>
+                       
                     </Grid>
 
                     <Grid>
@@ -130,7 +305,7 @@ export default function ProductCreationForm() {
                             multiline
                             
                             value={formik.values.description}
-                            onChange={formik.onChange}
+                            onChange={formik.handleChange}
 
                             sx={{
                                 width: 460
