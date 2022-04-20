a15aedb4cf4f64e7810c008e1a9f3eb12900d8dd
Jeremiah <jere.prasetyo.jp@gmail.com>
2022-04-21 00:09:07 +0700

    FE BE Product Lists CRUD

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -40,7 +40,7 @@ const routes = [
     { path: '/adminList', element: <AdminList /> },
     { path: '/admin/edit/account/:id', element: <AdminAccountEdit /> },
     { path: '/admin/create/account/', element: <AdminRegisterAccount /> },
-    { path: '/productCreation', element: <ProductCreation /> },
+    { path: '/product/create/', element: <ProductCreation /> },
     { path: '/productUpdate/:id', element: <ProductUpdate /> }
 ]
 
