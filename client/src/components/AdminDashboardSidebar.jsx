f241c87b1fdaa627767de2977eb9767863fe78d9
jepra777 <jere.prasetyo.jp@gmail.com>
2022-05-17 19:16:36 +0700

    Tidy up the User and Admin Sidebar

--- client/src/components/AdminDashboardSidebar.jsx
+++ client/src/components/AdminDashboardSidebar.jsx
@@ -1,14 +1,11 @@
 import { Grow } from "@material-ui/core";
-import { Drafts } from "@mui/icons-material";
-import { Inbox } from "@mui/icons-material";
-import { Box } from "@mui/material";
+import { SupervisorAccount, Home, Inventory, SettingsApplications, Forum, Insights } from "@mui/icons-material";
 import { Paper } from "@mui/material";
 import { ListItem } from "@mui/material";
 import { ListItemText } from "@mui/material";
 import { ListItemIcon } from "@mui/material";
 import { ListItemButton } from "@mui/material";
 import { List } from "@mui/material";
-import { Grid, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import '../styles/Dashboard.css'
 
@@ -16,26 +13,70 @@ export default function AdminDashboardSidebar() {
 
     return(
 
-            <Paper  style={{height:'calc(100vh - 64px)'}}>
-                 <List>
+      <Paper  style={{height:'calc(100vh - 64px)'}}>
+        <List>
           <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <Inbox />
-              </ListItemIcon>
-              <ListItemText primary="Inbox" sx={{display:{xs:"none", sm:"unset"}}} />
-            </ListItemButton>
+            <Link to="/">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Home />
+                </ListItemIcon>
+                <ListItemText primary="Home" sx={{display:{xs:"none", sm:"unset"}}} />
+              </ListItemButton>
+            </Link>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <Drafts />
-              </ListItemIcon>
-              <ListItemText primary="Drafts" sx={{display:{xs:"none", sm:"unset"}}} />
-            </ListItemButton>
+            <Link to="#">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Insights />
+                </ListItemIcon>
+                <ListItemText primary="Insights" sx={{display:{xs:"none", sm:"unset"}}} />
+              </ListItemButton>
+            </Link>
+          </ListItem>
+          <ListItem disablePadding>
+            <Link to="/adminList">
+              <ListItemButton>
+                <ListItemIcon>
+                  <SupervisorAccount />
+                </ListItemIcon>
+                <ListItemText primary="Admin Manager" sx={{display:{xs:"none", sm:"unset"}}} />
+              </ListItemButton>
+            </Link>
+          </ListItem>
+          <ListItem disablePadding>
+            <Link to="/productList">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Inventory />
+                </ListItemIcon>
+                <ListItemText primary="Product Manager" sx={{display:{xs:"none", sm:"unset"}}} />
+              </ListItemButton>
+            </Link>
+          </ListItem>
+          <ListItem disablePadding>
+            <Link to="#">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Forum />
+                </ListItemIcon>
+                <ListItemText primary="Chat Rooms" sx={{display:{xs:"none", sm:"unset"}}} />
+              </ListItemButton>
+            </Link>
+          </ListItem>
+          <ListItem disablePadding>
+            <Link to="#">
+              <ListItemButton>
+                <ListItemIcon>
+                  <SettingsApplications />
+                </ListItemIcon>
+                <ListItemText primary="Web Settings" sx={{display:{xs:"none", sm:"unset"}}} />
+              </ListItemButton>
+            </Link>
           </ListItem>
         </List>
-            </Paper>
+      </Paper>
 
 
             // <Paper  sx={{minHeight:"100vh",height:"100%",background:"orange"}}>
--- client/src/pages/AdminList.jsx
+++ client/src/pages/AdminList.jsx
@@ -1,37 +1,78 @@
+import { Grow } from "@mui/material";
+import React,{ useState } from "react";
 import { Button, Grid, Typography, Card, CardContent } from "@mui/material";
 import DashboardNavbar from "../components/DashboardNavbar";
 import AdminDashboardSidebar from '../components/AdminDashboardSidebar'
 import AdminTable from "../components/AdminTable";
 
 export default function AdminList() {
+    const [isToggle, setIsToggle] = useState(true);
+    const Component = React.forwardRef((props, ref) => {
+      return (
+        <div ref={ref} {...props}>
+         < AdminDashboardSidebar />
+        </div>
+      );
+    })
+    const handleToggle = (e) => {
+      setIsToggle(e);
+    };
     return (
-        <>
-            <Grid container display={'flex'}>
-                <Grid marginRight={'220px'}>
-                    <AdminDashboardSidebar />
-                </Grid>
+        // <>
+        //     <Grid container display={'flex'}>
+        //         <Grid marginRight={'220px'}>
+        //             <AdminDashboardSidebar />
+        //         </Grid>
 
-                <Grid container display={'flex'} alignItems={'center'}>
-                    <Grid marginLeft={'120px'} paddingTop={'30px'}>
-                        <Typography variant={'h5'} marginLeft={'200px'}>Admin List</Typography>
-                    </Grid>
+        //         <Grid container display={'flex'} alignItems={'center'}>
+        //             <Grid marginLeft={'120px'} paddingTop={'30px'}>
+        //                 <Typography variant={'h5'} marginLeft={'200px'}>Admin List</Typography>
+        //             </Grid>
 
-                    <Grid ml={'auto'}>
-                        <DashboardNavbar />
-                    </Grid>
-                </Grid>
-            </Grid>
+        //             <Grid ml={'auto'}>
+        //                 <DashboardNavbar />
+        //             </Grid>
+        //         </Grid>
+        //     </Grid>
  
-            <Grid display={'flex'} justifyContent={'center'} marginTop={'50px'} marginLeft={'150px'}>
-                <Card>
-                    <CardContent>
-                        <AdminTable />
-                        <Button  variant="contained" href={"/admin/create/account"}>
-                            Create Admin
-                        </Button>
-                    </CardContent>
-                </Card>
-            </Grid>
-        </>
+        //     <Grid display={'flex'} justifyContent={'center'} marginTop={'50px'} marginLeft={'150px'}>
+        //         <Card>
+        //             <CardContent>
+        //                 <AdminTable />
+        //                 <Button  variant="contained" href={"/admin/create/account"}>
+        //                     Create Admin
+        //                 </Button>
+        //             </CardContent>
+        //         </Card>
+        //     </Grid>
+        // </>
+    <Grid container>
+      <Grid item xs={12}>
+        <DashboardNavbar toggle={handleToggle} />
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container>
+          <Grid
+            item
+            xs={2}
+            sx={{display:isToggle ? "none" : "unset"}}
+          >
+            <Grow in={!isToggle}>
+                <Component />
+            </Grow>     
+          </Grid>
+          <Grid item xs={isToggle ? 12 : 10}>
+            <Card sx={{minHeight:"calc(100vh - 64px)"}}>
+              <CardContent>
+              <AdminTable />
+                <Button  variant="contained" href={"/admin/create/account"}>
+                    Create Admin
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
     )
 }
